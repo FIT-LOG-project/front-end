af98df8632f4a76d898080f73bdccb06146a7523
sangwoo <lesw1216@gmail.com>
2024-05-18 21:01:09 +0900

    feat: 신규 회원 닉네임 등록 구현

--- middleware/authenticateUser.global.ts
+++ middleware/authenticateUser.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    const userStore = useUserStore()
+    const authStore = useAuthStore()
 
     /**
      * /login, /sign-up 이 아닌 url 값이 들어 온 경우 인증 되지 않은 사용자면 /login로 이동
@@ -7,7 +7,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
     if (to.fullPath != "/login" && to.fullPath != "/sign-up") {
         
         // 로그인 하지 않은 사용자
-        if (!userStore.getIsAuthentication()) {
+        if (!authStore.getIsAuthentication()) {
             return navigateTo('/login')
         }
 
@@ -15,7 +15,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
          * "/"를 요청하면 "/training"으로 이동한다.
          */
         if (to.fullPath == "/") {
-            return navigateTo("/training")
+            return navigateTo("/weight")
         }
     }
 })
